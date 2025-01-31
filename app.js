dd7c7aa9b453da7ce74e4b2fb294d1e36ac9c3a8
Claire <c.tregunna1980@gmail.com>
2025-01-31 15:16:13 +0000

    app wokring, picks colors and modes

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const generateColourBtn = document.getElementById('generate-btn')
 const colorContainer = document.getElementById('color-container')
 const hexContainer = document.getElementById('hex-container')
+const colorPicker = document.getElementById('color-picker')
+const modeSelection = document.getElementById('mode-selection')
+
 
 
 generateColourBtn.addEventListener('click', function(e) {
     e.preventDefault()
-    fetch("https://www.thecolorapi.com/scheme?hex=0047AB&mode=monochrome&count=5")
-    .then(res => res.json())
-    .then(data => {
-        const generateColors = data.colors
-        generateColors.forEach(color => {
-            const colorDiv = document.createElement('div')
-            colorDiv.classList.add('h-1/5', 'md:h-full', 'md:w-1/5')
-            colorDiv.style.backgroundColor = color.hex.value
-            colorContainer.appendChild(colorDiv)
-
-            hexContainer.innerHTML += `
-                <div class="h-1/5 md:w-1/5 md:h-24 flex justify-center items-center">${color.hex.value}</div>
-            `
-            console.log(color.hex.value)
+    colorContainer.innerHTML = ''
+    hexContainer.innerHTML = ''
+    const pickedColor = colorPicker.value
+    const pickedMode = modeSelection.value
+    console.log(modeSelection.value)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${pickedColor.slice(1)}&mode=${pickedMode}&count=5`)
+        .then(res => res.json())
+        .then(data => {
+            const generateColors = data.colors
+            generateColors.forEach(color => {
+                // create the colour divs
+                const colorDiv = document.createElement('div')
+                colorDiv.classList.add('h-1/5', 'md:h-full', 'md:w-1/5')
+                colorDiv.style.backgroundColor = color.hex.value
+                colorContainer.appendChild(colorDiv)
+                // display the hex colds of the above colours
+                hexContainer.innerHTML += `
+                    <div class="h-1/5 md:w-1/5 md:h-24 flex justify-center items-center">${color.hex.value}</div>
+                `
+            })
         })
-    })
 })
